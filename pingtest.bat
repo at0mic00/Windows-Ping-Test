2307790a2814709193b0a1d0a1b3a58cd44e28ef
Om Bhende <om@curtcreation.net>
2016-06-05 14:15:51 +0530

    Ping Test.
    It's simple ping test configuration using batch script (only works with
    windows). It makes a log folder and uploads the folder to the FTP Server
    you want. you are free to modify this resource freely! Cheers (Y) (Y)

---
+++ pingtest.bat
@@ -0,0 +1,23 @@
+@echo off
+SET dir=%cd%
+SET rand=%random%
+echo Hello! Welcome to at0mic's Pingtest!
+echo Please wait until we fetch all your information and upload it to cloud which will give you your details!
+echo ________________________________________________________________________________
+echo Fetching IPConfig and Important Data.
+mkdir logs
+ipconfig /all >> logs/ipconfig.log
+echo Done! Pinging to google.com now!
+ping google.com -n 10 >> logs/ping.log
+echo Done! Final Step: Fetching Task List...
+tasklist >> logs/tasklist.log
+echo Ping Test Successfull! Please wait until pingtest files will be uploaded...
+cd logs
+..\bin\7za.exe a ..\%rand% *.* > NUL
+cd ..
+bin\ncftpput.exe -u USER_ID -p USER_PASS SERVER_IP SERVER_DIR_TO_UPLOAD *.7z
+echo %cd%
+
+echo Tests has been successfully tested and uploaded to the cloud! Your ID: %rand%. Please contact at0mic for more information..
+pause
+
